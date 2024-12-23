c6259f1b25a5a43d5a90e4036da11ac1ede0b0d8
Тарас <marf51@mail.ru>
2024-12-23 22:57:08 +0800

    -

--- search.py
+++ search.py
@@ -4,7 +4,9 @@ import gpxpy
 import time
 
 def get_distance(pt_1: tuple, pt_2: tuple):
-    return geopy.distance.geodesic(pt_1, pt_2).m
+    start_time = time.time()
+    d = geopy.distance.geodesic(pt_1, pt_2).m
+    return d, time.time() - start_time
 
 if __name__ == '__main__':
 
@@ -12,19 +14,27 @@ if __name__ == '__main__':
     galina = (51.94419, 102.37698)
 
     start_time = time.time()
+    dcalc_time = 0
+    parse_time = 0
     for fname in os.listdir(pic_dir):
         fname = f'{pic_dir}/{fname}'
         # print(f'\n{fname}: ', end='')
         with  open(fname, 'r', encoding='utf-8') as fgpx:
+            parse_start_time = time.time()
             gpx = gpxpy.parse(fgpx)
+            parse_time += time.time() - parse_start_time
             for track in gpx.tracks:
                 for trkseg in track.segments:
                     for trkpt in trkseg.points:
                         pt = (float(trkpt.latitude), float(trkpt.longitude))
-                        # print(f"{trkpt.latitude}\t{trkpt.longitude}")
-                        if get_distance(galina, pt) < 50:
+                        d = get_distance(galina, pt)
+                        dcalc_time += d[1]
+                        if d[0] < 50:
                             print(fname)
                             break
-
-    print('Elapsed time: ', time.time() - start_time)
+    full_time = time.time() - start_time
+    dcalc_time_prc = round(100*dcalc_time/full_time,1)
+    parse_time_prc = round(100*parse_time/full_time,1)
+    print('Elapsed time: ', time.time() - start_time, 'Calc time: ', dcalc_time, 'Parse time: ', parse_time)
+    print(f'Elapsed time: {time.time() - start_time} Calc time: {dcalc_time}({dcalc_time_prc}%) Parse time: {parse_time}({parse_time_prc}%)')
         # print(round(get_distance(pt_1,pt_2),  0))
