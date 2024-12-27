6344a49ba107c84e18b2d001e59d079cf1c9c329
Тарас <marf51@mail.ru>
2024-12-27 21:37:56 +0800

    добавил sqr_region

--- search.py
+++ search.py
@@ -8,15 +8,60 @@ def get_distance(pt_1: tuple, pt_2: tuple):
     d = geopy.distance.geodesic(pt_1, pt_2).m
     return d, time.time() - start_time
 
+def get_sqr_region(pt: tuple, side_size: int):
+    d_lat = geopy.distance.geodesic(pt,(pt[0]+0.1, pt[1])).m
+    d_lon = geopy.distance.geodesic(pt,(pt[0], pt[1]+0.1)).m
+    print(f"{d_lat}m {d_lon}m")
+    # 0.1 нужно динамически поднастроить исходя из масштаба
+    delta_lat = 0.1*(side_size/2.0)/d_lat
+    delta_lon = 0.1*(side_size/2.0)/d_lon
+    return (round(pt[0]+delta_lat,6), round(pt[1]-delta_lon,6)), (round(pt[0]-delta_lat,6), round(pt[1]+delta_lon,6))
+
+def sqr_region_2gpx(path: str, region: tuple):
+    gpx = gpxpy.gpx.GPX()
+    gpx_track = gpxpy.gpx.GPXTrack()
+    gpx.tracks.append(gpx_track)
+
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+
+    # Create points:
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(region[0][0], region[0][1]))
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(region[0][0], region[1][1]))
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(region[1][0], region[1][1]))
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(region[1][0], region[0][1]))
+    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(region[0][0], region[0][1]))
+    with open(path, 'w', encoding='utf-8') as gpx_to_file:
+        gpx_to_file.write(gpx.to_xml())
+
+def is_in_sqr_region(region: tuple, pt: tuple):
+    start_time = time.time()
+    if pt[0] > region[1][0]:
+        if pt[0] < region[0][0]:
+            if pt[1] > region[0][1]:
+                if pt[1] < region[1][1]:
+                    return True, time.time() - start_time
+    return False, time.time() - start_time
+
 if __name__ == '__main__':
 
     pic_dir = 'angara'
     galina = (51.94419, 102.37698)
     lovepeak = (51.94541, 102.43996)
+    munku =(51.71883, 100.59706)
+    vit = (51.97477, 104.10470)
+
+
+
 
     start_time = time.time()
     dcalc_time = 0
     parse_time = 0
+
+    reg_to_find = get_sqr_region(vit, 20)
+    sqr_region_2gpx('enisey/mumu.gpx', reg_to_find)
+
+
     for fname in os.listdir(pic_dir):
         fname = f'{pic_dir}/{fname}'
         # print(f'\n{fname}: ', end='')
@@ -28,14 +73,13 @@ if __name__ == '__main__':
                 for trkseg in track.segments:
                     for trkpt in trkseg.points:
                         pt = (float(trkpt.latitude), float(trkpt.longitude))
-                        d = get_distance(lovepeak, pt)
+                        d = is_in_sqr_region(reg_to_find, pt)
                         dcalc_time += d[1]
-                        if d[0] < 50:
+                        if d[0]:
                             print(fname)
                             break
     full_time = time.time() - start_time
     dcalc_time_prc = round(100*dcalc_time/full_time,1)
     parse_time_prc = round(100*parse_time/full_time,1)
-    print('Elapsed time: ', time.time() - start_time, 'Calc time: ', dcalc_time, 'Parse time: ', parse_time)
     print(f'Elapsed time: {time.time() - start_time} Calc time: {dcalc_time}({dcalc_time_prc}%) Parse time: {parse_time}({parse_time_prc}%)')
         # print(round(get_distance(pt_1,pt_2),  0))
