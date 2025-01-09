5faa9f996954a96fc4e14213c22d637f5a9c55d1
Тарас <marf51@mail.ru>
2025-01-09 16:31:52 +0800

    добавил regexp-ов

--- scache.py
+++ scache.py
@@ -14,38 +14,42 @@ def str_clean(s: str):
     pr = (
         'на', 'по', 'из', 'от', 'за', 'до', 'перед', 'без', 'через', 'над', 'про',
         'под', 'для', 'после', 'при', 'между', 'около', 'среди', 'вокруг', 'мимо',
-        'возле', 'вдоль',
+        'возле', 'вдоль', 'спереди', 'слева', 'справа,' 'сзади',
     )
     mon = ('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
-           'янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек')
+           'янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек',
+           )
 
     strava = ('strava', 'by', 'stravatogpx', 'app')
-    remove_symbols_queue0 = ',;\'=()!?+[]<>{}|*&^%$#@^|~'
-    remove_symbols_queue1 = ':_-./\\'
-    replace_symbols = '_'
+    remove_symbols_queue0 = ',;:\'=()!?[]<>{}|*&^%$#@^|~_+-/\\'
 
     for symbol in remove_symbols_queue0:
-        s = s.replace(symbol, '')
-    # for symbol in replace_symbols:
-    #     s = s.replace(symbol, ' ')
+        s = s.replace(symbol, ' ')
+        s = re.sub(r'\d{1,4}', r'', s)
+
     for word in s.lower().split():
+        word = re.sub(r'^\d{1,4}', r'', word)
         # word = re.sub(r'ст\.(\w+)', r'старая \1', word)
         # word = re.sub(r'ст\.', r'старая', word)
         word = re.sub(r'^ск\.(\w+)', r'скал \1', word)
         word = re.sub(r'^ск\.', r'скал', word)
-        word = re.sub(r'^п\.', r'пик', word)
         word = re.sub(r'^п\.(\w+)', r'пик \1', word)
-        word = re.sub(r'^пер\.', r'перевал', word)
+        word = re.sub(r'^п\.', r'пик', word)
         word = re.sub(r'^пер\.(\w+)', r'перевал \1', word)
-        word = re.sub(r'^оз\.', r'озеро', word)
+        word = re.sub(r'^пер\.', r'перевал', word)
         word = re.sub(r'^оз\.(\w+)', r'озеро \1', word)
-        word = re.sub(r'^р\.', r'река', word)
+        word = re.sub(r'^оз\.', r'озеро', word)
         word = re.sub(r'^р\.(\w+)', r'река \1', word)
-        word = re.sub(r'\d{4}[_.-]\d\d[_.-]\d\d', r'', word)
-        word = re.sub(r'\d\d[_.-]\d\d[_.-]\d{4}', r'', word)
-        word = re.sub(r'\d\d[:-]\d\d([:-]\d\d)?', r'', word)
+        word = re.sub(r'^р\.', r'река', word)
+        word = re.sub(r'^руч\.(\w+)', r'ручей \1', word)
+        word = re.sub(r'^руч\.', r'ручей', word)
 
-        word = re.sub(r'^(\w+)[.:]$', r'\1', word)
+
+        # word = re.sub(r'\d{4}[_.-]\d\d[_.-]\d\d', r'', word)
+        # word = re.sub(r'\d\d[_.-]\d\d[_.-]\d{4}', r'', word)
+        # word = re.sub(r'\d\d[:-]\d\d([:-]\d\d)?', r'', word)
+
+        # word = re.sub(r'^(\w+)[.:]$', r'\1', word)
 
         # is_time = re.match('\d{4}[_.-]\d\d[_.-]\d\d', word) \
         #           or re.match('\d\d[:-]\d\d([:-]\d\d)?', word) \
