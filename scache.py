68f5585b9fd751d51546e4f8fb08bc9abc65d204
Тарас <marf51@mail.ru>
2025-01-09 10:22:59 +0800

    добавил regexp-ов

--- scache.py
+++ scache.py
@@ -10,36 +10,52 @@ from gpxpy.gpx import GPX
 # перевал пер. пик п. гора
 
 def str_clean(s: str):
-    res = ''
-    pr = [
+    res = list()
+    pr = (
         'на', 'по', 'из', 'от', 'за', 'до', 'перед', 'без', 'через', 'над', 'про',
         'под', 'для', 'после', 'при', 'между', 'около', 'среди', 'вокруг', 'мимо',
         'возле', 'вдоль',
-    ]
-    strava = ['strava', 'by', 'stravatogpx', 'app']
-    remove_symbols = ',()!?+[]/<>{}|*&^%$#@'
+    )
+    mon = ('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
+           'янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек')
+
+    strava = ('strava', 'by', 'stravatogpx', 'app')
+    remove_symbols_queue0 = ',;\'=()!?+[]<>{}|*&^%$#@^|~'
+    remove_symbols_queue1 = ':_-./\\'
     replace_symbols = '_'
 
-    for symbol in remove_symbols:
+    for symbol in remove_symbols_queue0:
         s = s.replace(symbol, '')
-    for symbol in replace_symbols:
-        s = s.replace(symbol, ' ')
+    # for symbol in replace_symbols:
+    #     s = s.replace(symbol, ' ')
     for word in s.lower().split():
-        word = re.sub(r'ст\.(\w+)', r'старая \1', word)
-        word = re.sub(r'ст\.', r'старая', word)
-        word = re.sub(r'ск\.(\w+)', r'скал \1', word)
-        word = re.sub(r'ск\.', r'скал', word)
-        word = re.sub(r'п\.', r'пик', word)
-        word = re.sub(r'п\.(\w+)', r'пик \1', word)
-        word = re.sub(r'пер\.', r'перевал', word)
-        word = re.sub(r'пер\.(\w+)', r'перевал \1', word)
+        # word = re.sub(r'ст\.(\w+)', r'старая \1', word)
+        # word = re.sub(r'ст\.', r'старая', word)
+        word = re.sub(r'^ск\.(\w+)', r'скал \1', word)
+        word = re.sub(r'^ск\.', r'скал', word)
+        word = re.sub(r'^п\.', r'пик', word)
+        word = re.sub(r'^п\.(\w+)', r'пик \1', word)
+        word = re.sub(r'^пер\.', r'перевал', word)
+        word = re.sub(r'^пер\.(\w+)', r'перевал \1', word)
+        word = re.sub(r'^оз\.', r'озеро', word)
+        word = re.sub(r'^оз\.(\w+)', r'озеро \1', word)
+        word = re.sub(r'^р\.', r'река', word)
+        word = re.sub(r'^р\.(\w+)', r'река \1', word)
+        word = re.sub(r'\d{4}[_.-]\d\d[_.-]\d\d', r'', word)
+        word = re.sub(r'\d\d[_.-]\d\d[_.-]\d{4}', r'', word)
+        word = re.sub(r'\d\d[:-]\d\d([:-]\d\d)?', r'', word)
 
-        is_time = re.match('\d{4}-\d\d-\d\d', word) or re.match('\d\d[:-]\d\d([:-]\d\d)?', word) \
-                  or re.match('\d\d\.\d\d.\d{4}', word)
+        word = re.sub(r'^(\w+)[.:]$', r'\1', word)
+
+        # is_time = re.match('\d{4}[_.-]\d\d[_.-]\d\d', word) \
+        #           or re.match('\d\d[:-]\d\d([:-]\d\d)?', word) \
+        #           or re.match('\d\d[_.-]\d\d[_.-]\d{4}', word)
+
         if len(word) > 1:
-            if word not in pr and word not in strava and not is_time:
-                res = f'{res} {word}'
-    return res
+            # if word not in pr and word not in strava and word not in mon and not is_time:
+            if word not in [*pr, *strava, *mon]:
+                res.append(word)
+    return ' '.join(res)
 
 
 def index_gpx(fname: str, gpx: GPX):
