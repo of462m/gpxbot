61d978a1dd15d2824af0beb519b843c7e28fc038
Тарас <marf51@mail.ru>
2025-01-27 14:10:38 +0800

    -

--- solib.py
+++ solib.py
@@ -1,28 +1,30 @@
 import ctypes
 import os
 
+
 class wpt(ctypes.Structure):
     _fields_ = [('lat', ctypes.c_double),
                 ('lon', ctypes.c_double),
-               ]
+                ]
+
 
 if __name__ == '__main__':
-
     test = ctypes.CDLL('/usr/lib/libgpxbaikal.so')
 
-#	test.deg2rad.restype = ctypes.c_void_p
-#	test.deg2rad.argtypes = [ctypes.POINTER(wpt)]
+    #	test.deg2rad.restype = ctypes.c_void_p
+    #	test.deg2rad.argtypes = [ctypes.POINTER(wpt)]
 
-	test.geo2_distance_m.restype = ctypes.c_double
-#	test.geo2_distance_m.argtypes = [ctypes.POINTER(wpt)]
+    test.geo2_distance_m.restype = ctypes.c_double
+    #	test.geo2_distance_m.argtypes = [ctypes.POINTER(wpt)]
 
-	w_pt = wpt(52.28832,104.24759)
+    w_pt = wpt(52.28832, 104.24759)
+
     test.deg2rad(ctypes.byref(w_pt))
-	print(w_pt.lat,w_pt.lon)
+    print(w_pt.lat, w_pt.lon)
 
-	trk = (wpt*2)()
-	trk[0] = wpt(52.28832,104.24759)
-	trk[1] = wpt(52.29832,104.25759)
-	print(test.geo2_distance_m(ctypes.byref(trk)))
+    trk = (wpt * 2)()
+    trk[0] = wpt(52.28832, 104.24759)
+    trk[1] = wpt(52.29832, 104.25759)
+    print(test.geo2_distance_m(ctypes.byref(trk)))
 
-	print(os.getcwd())
+    print(os.getcwd())
